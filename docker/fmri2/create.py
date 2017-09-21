703784c85b5bb7f1c5edf98e58e5cd62bc7577e8
Dan Gale <danjgale@gmail.com>
2017-09-21 14:17:04 -0400

    initial commit

---
+++ docker/fmri2/create.py
@@ -0,0 +1,38 @@
+"""Neurodocker script to create DockerFile"""
+
+from neurodocker import Dockerfile
+import sys
+
+sys.path.append('../../envs/fmri')
+
+def set_specs():
+    specs = {
+        'pkg_manager': 'apt',
+        'check_urls': False,
+        'instructions': [
+            ('base', 'ubuntu:17.04'),
+            ('install', ['git', 'nano']),
+            ('miniconda', {
+                'env_name': 'fmri',
+                'conda_install': 
+                    'python=3.6 numpy scipy pandas jupyter scikit-learn matplotlib pytest scikit-image',
+                'pip_install': 'nipype nibabel nilearn nitime nipy'}),
+            ('afni', {'version': 'latest'}),
+            ('ants', {'version': '2.2.0'}),
+            ('freesurfer', {'version': '6.0.0', 'license_path': 'rel/path/license.txt'}),
+            ('fsl', {'version': '5.0.10', 'use_binaries': True}),
+            ('mrtrix3', {'use_binaries': False}),
+            ('neurodebian', {'os_codename': 'zesty', 'download_server': 'usa-nh',
+                            'pkgs': ['afni', 'dcm2niix']}),
+            ('spm', {'version': '12', 'matlab_version': 'R2017a'})
+            # ('copy', ['../../envs/fmri/emvironment.yml']),
+            # ('run', 'conda env create')
+        ]
+    }
+    return specs
+
+if __name__ == '__main__':
+
+    df = Dockerfile(set_specs())
+    df.save('Dockerfile')
+    print(df)
