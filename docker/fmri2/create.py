759fbf4e9d3063b959eecd8e3b3ec109d152c963
Dan Gale <danjgale@gmail.com>
2017-09-22 14:52:19 -0400

    minor changes to specs

--- docker/fmri2/create.py
+++ docker/fmri2/create.py
@@ -8,7 +8,7 @@ sys.path.append('../../envs/fmri')
 def set_specs():
     specs = {
         'pkg_manager': 'apt',
-        'check_urls': False,
+        'check_urls': True,
         'instructions': [
             ('base', 'ubuntu:17.04'),
             ('install', ['git gcc']),
@@ -16,14 +16,25 @@ def set_specs():
                 'env_name': 'fmri',
                 'conda_install': 
                     'python=3.6 numpy scipy pandas jupyter scikit-learn matplotlib pytest scikit-image',
-                    'pip_install': 'nipype nibabel nilearn nitime nipy'}),
+                    'pip_install': 'nipype nibabel nilearn nitime nipy'
+                    }
+            ),
             ('afni', {'version': 'latest'}),
             ('ants', {'version': '2.2.0'}),
-            ('freesurfer', {'version': '6.0.0', 'license_path': 'rel/path/license.txt'}),
+            ('freesurfer', {
+                'version': '6.0.0',
+                'license_path': 'rel/path/license.txt',
+                'min': True
+                }
+            ),
             ('fsl', {'version': '5.0.10', 'use_binaries': True}),
             ('mrtrix3', {'use_binaries': False}),
-            ('neurodebian', {'os_codename': 'zesty', 'download_server': 'usa-nh',
-                            'pkgs': ['afni', 'dcm2niix']}),
+            ('neurodebian', {
+                'os_codename': 'zesty', 
+                'download_server': 'usa-nh',
+                'pkgs': ['afni', 'dcm2niix']
+                }
+            ),
             ('spm', {'version': '12', 'matlab_version': 'R2017a'})
         ]
     }
